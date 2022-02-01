5dbe252942a8b9068dd71ccc165dbae756dba399
sobotta-python <Sobotta2008@outlook.de>
2022-02-01 21:52:58 +0100

    new button method

--- main.py
+++ main.py
@@ -6,9 +6,21 @@ from flask import Flask, url_for, render_template, request, redirect, session, e
 #app
 app = Flask(__name__)
 
-##route index page
-@app.route("/")
+#route index page
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    #Pressed Button
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Contact me!':
+            print("My Email is ...")
+            return render_template('index.html')
+        else:
+            pass #unknown
+    
+    #else
+    elif request.method == 'GET':
+        print("ERROR something went wrong")
+        return render_template('index.html')
     return render_template('index.html')
 
 #start
